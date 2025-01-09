16e8e14932e0418c52cead4a3e3abc1b891535cb
qiuxan <qiuxan@gmail.com>
2025-01-09 21:27:52 +1100

    Create User Model

---
+++ models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const userSchema = new mongoose.Schema(
+    {
+        name:String,
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            index:true,
+            lowercase:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        role:{
+            type:[String],
+            default:["subscriber"],
+        },
+        image:String,
+        resetCode:{
+            data:String,
+            expireAt:{
+                type:Date,
+                default:()=> new Date(Date.now() + 10 * 60 * 1000)
+                
+            }
+        }
+    },
+    {timestamps:true}
+);
+
+userSchema.plugin(mongooseUniqueValidator,{message:"{PATH} is already taken"});
+
+export default mongoose.models.User || mongoose.model("User",userSchema);
