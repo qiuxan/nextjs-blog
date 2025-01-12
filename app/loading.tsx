0c1b1c09b7a34cdc5fc553024d782e80788628f3
qiuxan <qiuxan@gmail.com>
2025-01-12 11:12:07 +1100

    create a loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-90 text-danger display-1 fw-bold">
+        loading
+    </div>
+  )
+}
+
+export default loading
