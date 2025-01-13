ef2be24f22241a42304d849f8381c46b5d3943c1
qiuxan <qiuxan@gmail.com>
2025-01-13 16:25:26 +1100

    setup Google Sign in information

--- config_example.js
+++ config_example.js
@@ -4,9 +4,13 @@ const API = process.env.NODE_ENV === "production"
   ? ""
   : "";
 const NEXTAUTH_SECRET = '';
+const GOOGLE_CLIENT_ID= '';
+const GOOGLE_CLIENT_SECRET= '';
 
 module.exports = {
   DB_URI,
   API,
-  NEXTAUTH_SECRET
+  NEXTAUTH_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
 };
